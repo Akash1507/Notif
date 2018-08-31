0639a225f2c26a3694381e1e6cecc4ebde2b5350
Akash Sharma <36261234+Akash1507@users.noreply.github.com>
2018-08-31 12:23:56 +0530

    Update constants.py

--- Notif/src/models/alerts/constants.py
+++ Notif/src/models/alerts/constants.py
@@ -1,6 +1,6 @@
 
 COLLECTION = "alerts"
 ALERT_TIMEOUT = 10
-URL = "http://api.mailgun.net/v3/sandboxe7bb5f6133af4b57a72bb25e4014d222.mailgun.org/messages"
-API_KEY = "key-dbf5bbd51dbeee0f8f77f9193a521351-c1fe131e-f17835a8"
-FROM = "Mailgun Sandbox postmaster@sandboxe7bb5f6133af4b57a72bb25e4014d222.mailgun.org"
+URL = "Your Url"
+API_KEY = "Your Api Key"
+FROM = "Your User"
